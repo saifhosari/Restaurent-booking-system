a1ba9aecd2736ebe0fa29a7d9cb82e2b30d484fa
saifhosari <saifhalab93@gmail.com>
2023-05-17 22:53:29 +0500

    Update urls.py

--- restaurent/urls.py
+++ restaurent/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('restaurent_app.urls')),
+    
 ]
+
+urlpatterns = [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
