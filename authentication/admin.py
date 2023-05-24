a41b32eead16a83384dd53e557b655a880eba9bf
saifhosari <saifhalab93@gmail.com>
2023-05-24 09:39:50 +0500

    Added some models in django admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from authentication.models import *
 # Register your models here.
+@admin.register(Table)
+class addTable(admin.ModelAdmin):
+    list_display = ["table_name", "profile_username"]
 
-admin.site.register(Table)
+    def profile_username(self, obj):
+        return obj.profile.username
+
+    profile_username.short_description = "Profile Username"
+
 admin.site.register(Booking)
 admin.site.register(Profile)
 admin.site.register(Guest)
