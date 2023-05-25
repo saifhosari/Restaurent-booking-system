6b52bfc0fa6b80479622e25c53a36f6710e1af98
saifhosari <saifhalab93@gmail.com>
2023-05-25 21:21:42 +0500

    in progress

--- restaurent_app/static/js/login.js
+++ restaurent_app/static/js/login.js
@@ -1,7 +1,7 @@
 
     $('#login_form').on('submit', function(event) {
         event.preventDefault();  // Prevent normal form submission
-          alert("in login")
+
         // Submit the form via AJAX using the ajaxSubmit() function
         $(this).ajaxSubmit({
           url: login_url,  // Replace 'register' with your actual URL name
