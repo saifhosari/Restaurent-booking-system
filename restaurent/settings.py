561829a8ec12e1887c7f91036a279bcd109bc7db
saifhosari <saifhalab93@gmail.com>
2023-05-27 02:56:14 +0500

    Update settings.py

--- restaurent/settings.py
+++ restaurent/settings.py
@@ -110,7 +110,7 @@ else:
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
-}
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
