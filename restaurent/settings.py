0d7c1a893b8efbf5d2f428d701fb524d3054601f
saifhosari <saifhalab93@gmail.com>
2023-05-28 16:06:35 +0500

    Update settings.py

--- restaurent/settings.py
+++ restaurent/settings.py
@@ -79,18 +79,19 @@ WSGI_APPLICATION = 'restaurent.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 if not DEBUG:
     import dj_database_url
     db_from_env = dj_database_url.config(conn_max_age=600)
     DATABASES['default'].update(db_from_env)
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
